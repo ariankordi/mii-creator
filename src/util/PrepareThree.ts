2dc3b288192b7b2edb40c79d3391681aa6571c7a
JoshuaTheNewUser <mediomayorpudu2021@gmail.com>
2026-08-27 22:39:03 -0400

    Update PrepareThree.ts

--- src/util/PrepareThree.ts
+++ src/util/PrepareThree.ts
@@ -6,9 +6,7 @@ if (globalThis.THREE) {
   console.log("found three.js in global!");
 } else {
   console.log("oops, i didn't find three.js!", globalThis.THREE);
-  const three = (
-    await import(new URL("./three.js", import.meta.url).href)
-  ).default;
+  const three = (await import("./three.js")).default;
 
   THREE = three;
 }
